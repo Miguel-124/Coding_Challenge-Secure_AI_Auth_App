449a2e3f8552e77482bae30200f3402dbc6d608d
michalgorecki <michalgorecki124@gmail.com>
2025-06-14 11:30:32 +0200

    add challenge routing and improve CORS middleware formatting

--- backend/src/app.py
+++ backend/src/app.py
@@ -1,14 +1,15 @@
 from fastapi import FastAPI, Request, Response
 from fastapi.middleware.cors import CORSMiddleware
-from clerk_backend_api import Clerk
-import os
-
-clerk_sdk = Clerk(bearer_auth=os.getenv("CLERK_SECRET_KEY"))
+from .routes import challenge
 
 app = FastAPI()
 
-app.add_middleware(CORSMiddleware,
-                   allow_origins=["*"],
-                   allow_credentials=True,
-                   allow_methods=["*"],
-                   allow_headers=["*"])
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
+
+app.include_router(challenge.router, prefix="/api")
